0438d1f393baa3eda8d605a1cf7f6c7dfebdc32a
zortracks <zortracks@outlook.com>
2025-05-20 00:38:40 +0200

    Adding database support into aspire

---
+++ aspire/Zortracks.PsInfo.AppHost/Database.cs
@@ -0,0 +1,18 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+namespace Zortracks.PsInfo.AppHost {
+
+    public static class Database {
+        public static IResourceBuilder<MySqlServerResource> MySqlServerResource { get; private set; }
+        public static IResourceBuilder<MySqlDatabaseResource> PsInfoDatabase { get; private set; }
+
+        public static void Configure(IDistributedApplicationBuilder builder) {
+            MySqlServerResource = builder.AddMySql("mysql-server")
+                .WithDataVolume("mysql-data")
+                .WithPhpMyAdmin(configureContainer => configureContainer.PublishAsContainer());
+
+            PsInfoDatabase = MySqlServerResource.AddDatabase("ps-info-database", "ps-info");
+        }
+    }
+}
